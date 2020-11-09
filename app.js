49ff0d25371d623d4cedbc0d354e8384e4e6bbc4
isauremgn <isauremignon@gmail.com>
2020-11-09 17:15:10 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,51 @@
+var firstName = 'Isaure';
+
+var favouriteColor =  'blue';
+
+var favouriteFood = 'Hamburger';
+var City = 'Paris'
+
+var x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log(x);
+
+var x = 'Laurel'
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+
+// This is for know me better 
+console.log(firstName, favouriteColor, favouriteFood);
+
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var  seven = 'violet';
+
+console.log(phrase + '',one + ',', two + ',', three + ',',four + ',', five +  ',' ,six + ',' , seven + '', endingPrase);
+
+var myAlphabetlength = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabetlength.length);
+
+var x = 5, y = 10, z = 15;
+x+y;
+y-x;
+x*y;
+y/x;
+x%2;
+x++;
+x--;
+
+
+var total = 5%2;
+console.log(total);
+
